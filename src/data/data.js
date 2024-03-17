416e76fa70fd7c675bd6f26de3c4a63e1fec75cf
Harsh <ph335579@gmail.com>
2024-03-17 10:36:27 +0530

    ok

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@ import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
 import Ebook from '../assets/ebook.png'
 import Jss from '../assets/jsss.png'
+import FF from '../assets/fastfood.png'
 export const data=[
     {
         id:1,
@@ -15,15 +16,15 @@ export const data=[
         id:2,
         name:"JavaScript Projects",
         image:Jss,
-        github:"https://github.com/patelharshshaileshbhai/Study-Material-App",
-        live:"",
+        github:"https://github.com/patelharshshaileshbhai/JavaScript-Projects",
+        live:"https://github.com/patelharshshaileshbhai/JavaScript-Projects",
     },
     {
         id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"Food Delivery App",
+        image:FF,
+        github:"https://github.com/patelharshshaileshbhai/FoodDeliverApp",
+        live:"https://food-deliver-app-5h5q.vercel.app/",
     },
     {
         id:4,
