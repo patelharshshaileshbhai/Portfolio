10520190b0de5fea0c8718de2b7b217fd27de510
Harsh <ph335579@gmail.com>
2024-08-08 12:46:24 +0530

    resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/18EDij4HGxs-fNSzcoRNDCJhiF6yY1__z/view'
+              href='https://drive.google.com/file/d/192bn0kezcNWHTTWhSAaRIL6huo3gi6_U/view?usp=sharing'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
